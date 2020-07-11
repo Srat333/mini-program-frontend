cfb3a6123b55305a80711401ec2df77455838eb7
yngyuan <zedd.yuan@utexas.edu>
2020-07-11 13:48:50 -0500

    post question ok

--- pages/question/question.js
+++ pages/question/question.js
@@ -76,6 +76,7 @@ Page({
     }
   },
 
+  // 图片模块
   chooseImage(e) {
     wx.chooseImage({
       count: 3,
@@ -105,19 +106,54 @@ Page({
     })
   },
 
+
+createQuestion(e) {
+  wx.request({
+    url : "http://192.168.1.15:8080/qa/add",
+    method: "POST",
+    data: {
+      title : JSON.stringify(this.data.title),
+      content: JSON.stringify(this.data.content),
+      category: JSON.stringify(this.data.catagory),
+      tag: JSON.stringify(this.data.tag),
+    },
+    header: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    success: function (res) {
+      console.log(res.data);
+      wx.navigateBack({
+        delta: 1  //小程序关闭当前页面返回上一页面
+      })
+      wx.showToast({
+        title: '请教成功！',
+        icon: 'success',
+        duration: 2000
+      })
+    },
+  })
+},
+
+  // 提交表单
   submitForm(e) {
     const title = this.data.title
     const content = this.data.content
 
     if (title && content) {
       const arr = []
-      
+
       //将选择的图片组成一个Promise数组，准备进行并行上传
       for (let path of this.data.images) {
+        console.log("getting image path");
+        console.log(path);
         arr.push(wxUploadFile({
-          url: 'http://192.168.1.15:8080' + '/qa/',
+          url: 'https://api.imgbb.com/1/upload',
           filePath: path,
           name: 'qimg',
+          formData: {
+            key: 'b497dc0c5902318efcee4eebe39ccdba',
+            image: path,
+          },
         }))
       }
 
@@ -134,22 +170,15 @@ Page({
         console.log(">>>> upload images error:", err)
       }).then(urls => {
         // 调用保存问题的后端接口
-        return createQuestion({
-          title: title,
-          content: content,
-          images: urls
-        })
+        return this.createQuestion();
+        // console.log("getting url on image server");
+        // urls.forEach(url => {
+        //   console.log(url);
+        // });
       }).then(res => {
-        // 保存问题成功，返回上一页（通常是一个问题列表页）
-        const pages = getCurrentPages();
-        const currPage = pages[pages.length - 1];
-        const prevPage = pages[pages.length - 2];
-
-        // 将新创建的问题，添加到前一页（问题列表页）第一行
-        prevPage.data.questions.unshift(res)
-        $digest(prevPage)
-
-        wx.navigateBack()
+        wx.showToast({
+          title: 'success',
+        })
       }).catch(err => {
         console.log(">>>> create question error:", err)
       }).then(() => {
